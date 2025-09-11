7b9f48dc0ab6ba512f1a0da90b2a313e617bb358
Theo Vigano <135166649+TheoVigano@users.noreply.github.com>
2025-09-11 19:23:47 -0300

    Add files via upload

--- pooII.sql
+++ pooII.sql
@@ -14,6 +14,45 @@ INSERT INTO tbusuario(nome, email, senha, datanasc, ativo) VALUES
 ('theo', 'theo@gmail.com', 'musica', '2007-08-13', true);
 UPDATE tbusuario SET senha = SHA1('musica');
 
+CREATE TABLE tbartista (
+    pkartista INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    genero VARCHAR(40),
+    pais_origem VARCHAR(40)
+);
+
+CREATE TABLE tbmusica (
+    pkmusica INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    duracao TIME,
+    genero VARCHAR(40),
+    ano INT
+);
+
+CREATE TABLE tbplaylist (
+    pkplaylist INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    fkusuario INT,
+    FOREIGN KEY (fkusuario) REFERENCES tbusuario(pkusuario)
+);
+
+CREATE TABLE tbmusica_artista (
+    fk_musica INT,
+    fk_artista INT,
+    PRIMARY KEY (fk_musica, fk_artista),
+    FOREIGN KEY (fk_musica) REFERENCES tbmusica(pkmusica),
+    FOREIGN KEY (fk_artista) REFERENCES tbartista(pkartista)
+);
+
+CREATE TABLE tbmusica_playlist (
+    fk_musica INT,
+    fk_playlist INT,
+    PRIMARY KEY (fk_musica, fk_playlist),
+    FOREIGN KEY (fk_musica) REFERENCES tbmusica(pkmusica),
+    FOREIGN KEY (fk_playlist) REFERENCES tbplaylist(pkplaylist)
+);
+
+
 ALTER TABLE tbusuario ADD imagem BLOB;
 
 
